5558ec801b175a027c060dca11a63dae281d88b8
bxsck <bosstk05nya@gmail.com>
2021-05-12 23:45:08 +0700

    change migrate path

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-var ItemManager = artifacts.require("./ItemManager.sol");
+var ItemManager = artifacts.require("ItemManager.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ItemManager);
